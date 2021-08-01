1edd2ebb10b1a1426a671dc8ec2136feba3cbbc7
patriciasilva.eq@gmail.com <patriciasilva.eq@gmail.com>
2021-07-31 21:05:02 -0400

    Clique na carta

--- script.js
+++ script.js
@@ -7,9 +7,16 @@ let totalCards;
      "src='assets/revertitparrot.gif'",
      "src='assets/unicornparrot.gif'",
      "src='assets/tripletsparrot.gif'"
- ]
+ ] 
 
 let pairsCards = [0, 0];
+let checked1;
+let checked2;
+let clickCount = 1;
+let backFaceSelection1;
+let frontFaceSelection1;
+let backFaceSelection2;
+let frontFaceSelection2;
 
 backFaces.sort(comparator);
 
@@ -17,24 +24,27 @@ function comparator(){
     return 0.5 - Math.random();
 }
 
-function duplicateCards(){
+function duplicateCards() {
     let doubleCards = [];
-    for(let i = 0; i < totalCards/2; i++){
+    for(let i = 0; i < totalCards/2; i++) {
         doubleCards.push(backFaces[i]);
         doubleCards.push(backFaces[i]);
     }
+
     doubleCards.sort(comparator);
     return doubleCards;
 }
 
 function startGame() {     
-    totalCards = Number(prompt("Com quantas cartas quer jogar?"));
+    totalCards = Number(prompt("Com quantas cartas quer jogar?"));    
     while ((totalCards % 2 !== 0) || (totalCards < 4) || (totalCards > 14)) {
         totalCards = Number(prompt("Com quantas cartas quer jogar?"));
     }
+
     let container = document.querySelector(".container");
     let doubleCards = duplicateCards();
-    for(let i = 0; i < totalCards; i++){
+
+    for(let i = 0; i < totalCards; i++) {
         container.innerHTML += `
             <div class="card" onclick="cardRotation(this)">
                 <div class="front-face">
@@ -45,12 +55,52 @@ function startGame() {
                 </div>
             </div>
         `;
-    }
-        
+    }        
     duplicateCards();
 }
-
 startGame()
 
+function cardRotation (element) {    
+    if (clickCount % 2 !== 0) {       
+        checked1 = element;
+        checked1.classList.add("check1");   
+        checked1.classList.add("disabled"); 
+        backFaceSelection1 = document.querySelector(".card.check1 .back-face");
+        backFaceSelection1.classList.add("back-face-rotation");
+        frontFaceSelection1 = document.querySelector(".card.check1 .front-face");
+        frontFaceSelection1.classList.add("front-face-rotation");
+        pairsCards[0] = document.querySelector(".card.check1 .back-face img").getAttribute("src");        
+    } else {
+        checked2 = element;
+        checked2.classList.add("check2");
+        checked2.classList.add("disabled");         
+        backFaceSelection2 = document.querySelector(".card.check2 .back-face");
+        backFaceSelection2.classList.add("back-face-rotation");
+        frontFaceSelection2 = document.querySelector(".card.check2 .front-face");
+        frontFaceSelection2.classList.add("front-face-rotation");        
+        pairsCards[1] = document.querySelector(".card.check2 .back-face img").getAttribute("src");        
+    } 
+
+    if (clickCount % 2 === 0) {
+        if (pairsCards[0]  === pairsCards[1]){
+            checked1.classList.remove("check1");  
+            checked2.classList.remove("check2");
+            checked2.classList.add("disabled");
+            checked1.classList.add("disabled");                           
+        } else {
+            setTimeout(function(){
+                backFaceSelection1.classList.remove("back-face-rotation");
+                frontFaceSelection1.classList.remove("front-face-rotation");
+                backFaceSelection2.classList.remove("back-face-rotation");
+                frontFaceSelection2.classList.remove("front-face-rotation");
+                checked1.classList.remove("check1");  
+                checked2.classList.remove("check2");
+                checked2.classList.remove("disabled");
+                checked1.classList.remove("disabled");    
+            }, 1000);
+        }
+    }
+        clickCount ++;        
+ }
 
 
